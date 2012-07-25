b54187aa0861b3b55bac835a95eb745e2f8e0669
Alon BT <alonbt@wix.com>
2012-07-25 15:41:18 +0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,67 +1,41 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe "Static pages" do 
   
   let(:base_title) { "Alon Bartur test app"}
   
-  describe "Home page" do
-
-    it "should have the right title" do 
-      visit root_path
-      page.should have_selector('title', 
-                                :text => "#{base_title}")
-    end
-
-    it "should have the content 'Sample App'" do
-      visit root_path
-      page.should have_content('Sample App')
-    end
-    
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
-
+  subject { page }
+  
+  describe "Home page" do   
+    before { visit root_path }
+       
+    it { should have_selector 'h1', text: "Sample App" }
+    it { should have_selector 'title', text: full_title('') } 
+    it { should_not have_selector 'title', text: '| Home' }
   end
   
   describe "Help page" do
-  
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title', 
-                                :text => "#{base_title} | Help")
-    end
+    before { visit help_path }
     
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
+    it {should have_selector 'title', text: full_title('Help') }
+    it {should have_content 'Help' }
   end
   
   describe "About page" do
-  
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title', 
-                                :text => "#{base_title} | About us")
-    end
-  
-    it "should have the content 'About us'" do
-      visit about_path
-      page.should have_content('About us')
-    end
+    
+    before { visit about_path }
+    
+    it {should have_selector 'title', text: full_title('About us') }
+    it {should have_content 'About us'}  
   end
   
   describe "Contact page" do
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title',
-                                 :text => "#{base_title} | Contact us")
-    end
-    it "should have the content 'Contact us'" do
-      visit contact_path
-      page.should have_content('Contact us') 
-    end
+    
+    before { visit contact_path }
+    
+    it {should have_selector 'title', text: full_title('Contact us') }
+    it {should have_content 'Contact us' }
+    
   end
   
 end
