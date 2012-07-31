c7698313a78a09aba1c122971f40746096195a09
Alon BT <alonbt@wix.com>
2012-07-31 17:45:30 +0300

    Make a basic User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   
   before_save { |user| user.email = email.downcase}
   
@@ -19,4 +21,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
 end
