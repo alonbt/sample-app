ee86bdf87899bb4102fe636eb85f4593a686b937
Alon Bartur <alonbt@wix.com>
2012-08-12 00:30:48 +0300

    Excersize of chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,15 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: "Sign up")}
+        it { should have_content('error') }
+        
+      end
+      
     end
     
     describe "with valid information" do
@@ -39,6 +48,14 @@ describe "User Pages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('alonbt@gmail.com') }
+        
+        it {should have_selector("title", text: user.name)}
+        it {should have_selector("div.alert.alert-success", text:"Welcome #{user.name}")}
       end
     end
   end
